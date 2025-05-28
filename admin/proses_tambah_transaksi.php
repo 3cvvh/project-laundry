e77e99cfaf41f0249c0385fd993444c9885b7116
3cvvh <ayane9012@gmail.com>
2025-05-28 22:23:41 +0700

    transaksi

--- admin/proses_tambah_transaksi.php
+++ admin/proses_tambah_transaksi.php
@@ -1,6 +1,7 @@
 <?php
 if($_POST){
-    $id_transaksi=$_POST['id_transaksi'];
+    $qty = $_POST['qty'];
+    $keterangan = $_POST['keterangan'];
     $id_member=$_POST['id_member'];
     $id_outlet=$_POST['id_outlet'];
     $id_paket=$_POST['id_paket'];
@@ -10,22 +11,25 @@ if($_POST){
     $status=$_POST['status'];
     $dibayar=$_POST['dibayar'];
 
-if(empty($id_transaksi)){
-    echo "<script>alert('Id transaksi tidak boleh kosong');location.href='transaksi.php';</script>";
-    } elseif(empty($id_member)){
-    echo "<script>alert('Id member tidak boleh kosong');location.href='transaksi.php';</script>";
-    } else {
-    include "koneksi.php";
-    $insert=mysqli_query($conn,"insert into transaksi
-    (id_transaksi, id_member, id_outlet, id_paket, id_user, tgl, batas_waktu, status, dibayar) 
-    value
-    ('".$id_transaksi."','".$id_member."','".$id_outlet."','".$id_paket."','".$id_user."','".$tgl."','".$batas_waktu."','".$status."','".$dibayar."')") or
-    die(mysqli_error($conn));
-if($insert){
-    echo "<script>alert('Sukses menambahkan transaksi');location.href='transaksi.php';</script>";
-} else {
-    echo "<script>alert('Gagal menambahkan transaksi');location.href='transaksi.php';</script>";
-}
-}
-}
+        include "koneksi.php";
+        if($dibayar == 'dibayar') {
+            $tgl_bayar = date('Y-m-d');
+        } else {
+            $tgl_bayar = '0000-00-00';
+        }
+        $query_insert="INSERT INTO transaksi VALUES('','$id_outlet','$id_member','$tgl','$batas_waktu','$tgl_bayar','$status','$dibayar','$id_user','$id_paket')";
+        $insert=mysqli_query($conn, $query_insert);
+        if($insert){
+            $id_transaksi = mysqli_insert_id($conn);
+            $query_detail = "INSERT INTO detail_transaksi VALUES('', '$id_transaksi', '$id_paket', '$qty', '$keterangan')";
+            $insert_detail = mysqli_query($conn, $query_detail);
+            if($insert_detail){
+                echo "<script>alert('Sukses menambahkan transaksi dan detail transaksi');location.href='transaksi.php';</script>";
+            } else {
+                echo "<script>alert('Transaksi berhasil, tapi gagal menambahkan detail transaksi');location.href='transaksi.php';</script>";
+            }
+        } else {
+            echo "<script>alert('Gagal menambahkan transaksi');location.href='transaksi.php';</script>";
+        }
+    }
 ?>
