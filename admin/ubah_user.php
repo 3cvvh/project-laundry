be88b93a2e559f4e7c316e408924f872937b8405
3cvvh <ayane9012@gmail.com>
2025-06-01 19:45:55 +0700

    khesya love rasya

--- admin/ubah_user.php
+++ admin/ubah_user.php
@@ -42,7 +42,7 @@ session_start();
                 </div>
                 <div>
                     <label class="block text-gray-700 text-sm mb-1 font-medium">Password<span class="text-red-500">*</span></label>
-                    <input type="text" name="password" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400 text-sm" placeholder="masukan password baruu" required>
+                    <input type="text" name="password" class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400 text-sm" value="<?php echo $data["password"] ?>" required>
                 </div>
                 <div>
                     <label class="block text-gray-700 text-sm mb-1 font-medium">Nama Outlet<span class="text-red-500">*</span></label>
